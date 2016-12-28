5db978615eb9c0b7382909556d0e523f314230c9
Matheus Fidelis <msfidelis@outlook.com>
2016-12-28 10:45:48 -0200

    Otimizações e Blackfire

--- Docker/PHP7/run.sh
+++ Docker/PHP7/run.sh
@@ -1,2 +1,2 @@
 #nohup php -S 0.0.0.0:80 -t foo/ /var/www/html > /tmp/phpd.log 2>&1 &
-cd /var/www/html; phpunit --debug --log-json /tmp/logs/php5.json;
+cd /var/www/html; sleep 2; phpunit --debug --log-json /tmp/logs/php7.json;
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker stop $(docker ps -q);
-sudo docker-compose -f docker-compose-php7.yml up --abort-on-container-exit &&
-sudo docker-compose -f docker-compose-php5.yml up --abort-on-container-exit
+docker stop $(docker ps -qa);
+sudo docker-compose -f docker-compose-php5.yml up --abort-on-container-exit &&
+sudo docker-compose -f docker-compose-php7.yml up --abort-on-container-exit
