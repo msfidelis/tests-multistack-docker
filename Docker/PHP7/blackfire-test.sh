f4ac25cbc8856d2f25c7fd0d434bce49b13871b1
Matheus Fidelis <msfidelis@outlook.com>
2016-12-29 16:40:09 -0200

    Novas files

---
+++ Docker/PHP7/blackfire-test.sh
@@ -0,0 +1,7 @@
+URI="/cursos/getcursos /cursos/getcursos /cursos/getcursobyid/6")
+
+for i in "${URI[@]}"
+do
+   echo $i
+
+done
---
+++ start-blackfire.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo docker-compose -f docker-compose-php7-blackfire.yml up --abort-on-container-exit
